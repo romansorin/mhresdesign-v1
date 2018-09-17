b97d0ddb48e3abf5ce3232e613a94e651962cf9b
Roman Sorin <988244@students.mentorschools.org>
2018-09-17 08:09:12 -0400

    initial commit to repo
    init

---
+++ guidance/index.php
@@ -0,0 +1,24 @@
+<head>
+    <title>Guidance | Mentor High School</title>
+    <style>
+    body {
+		font-family: 'Noto Serif JP', sans-serif;
+		height: 100%;
+		display: inline-block;
+		min-height: 100%;
+		min-width: 100%;
+	}
+	html {
+		height: 100%;	
+	}
+</style>
+</head>
+
+<?php include '../inc/header.php'; ?>
+
+<h1>Following will be on this webpage (not final)</h1>
+<h3>Guidance Resouces<br/>Resources Tab<br/>College, Naviance<br/></h3>
+<h5>admin.php, guidance directory, calendars</h5>
+
+
+<?php include '../inc/footer.php'; ?>
