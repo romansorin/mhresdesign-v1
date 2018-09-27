4db20c796db5f9d5e914cc685dcf88d324e166a7
Ryan Rapini <ryanrapini@gmail.com>
2018-09-26 20:06:11 -0400

    Final fixes for new responsive design

--- inc/header.php
+++ inc/header.php
@@ -1,41 +1,42 @@
-<div class = "totaltopnav">
-    <nav class="navbar navbar-default navbar-static-top justify-content-between" role = "navigation">
-        <div class="container-fluid">
-            <span class="restxt" style="color: #656256 !important;">Resources for:</span>
-            <ul class="nav navbar-nav topleft"> 
-                <li><a href="/sorin/students" class="hvr-overline-reveal">Students</a></li>
-                <li><a href="/sorin/faculty-staff" class="hvr-overline-reveal">Faculty & Staff</a></li>
-                <li><a href="/sorin/parents" class="hvr-overline-reveal">Parents</a></li>
-                <li><a href="/sorin/alumni" class="hvr-overline-reveal">Alumni</a></li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right topnavbar-right topright">
-                <li><a href="/sorin/directory">Directory</a></li>
-                <li><a href="/sorin/map">Campus Map</a></li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right topnavbar-right search">
-                <div class="search-container">
-                    <form action="/action_page.php">
-                        <input type="text" placeholder="Search.." name="search">
-                        <button type="submit"><i class="fa fa-search"></i></button>
-                    </form>
-                </div>
-                <!-- search goes here -->
-            </ul>
+<nav class="navbar navbar-expand-sm navbar-light">
+    <span class="navbar-brand d-none d-md-block">Resources for:</span>
+    <ul class="navbar-nav navbar-resources"> 
+        <li class="nav-item"><a href="/sorin/students" class="nav-link hvr-overline-reveal">Students</a></li>
+        <li class="nav-item"><a href="/sorin/faculty-staff" class="nav-link hvr-overline-reveal">Faculty & Staff</a></li>
+        <li class="nav-item"><a href="/sorin/parents" class="nav-link hvr-overline-reveal">Parents</a></li>
+        <li class="nav-item"><a href="/sorin/alumni" class="nav-link hvr-overline-reveal">Alumni</a></li>
+    </ul>
+    <ul class="nav navbar-nav ml-auto search">
+        <div class="search-container">
+            <form action="/action_page.php">
+                <input type="text" placeholder="Search.." name="search">
+                <button type="submit"><i class="fa fa-search"></i></button>
+            </form>
         </div>
-    </nav>
-    <nav class="navbar navbar-inverse navbar-static-top justify-content-center" role="navigation">
-        <div class="container-fluid">
-            <a class="brand-img" href="/sorin/index.php" title="Mentor High School Homepage">
-                <img class="center-brand" src="/sorin/inc/mentor.png" alt="Mentor High School heading"></a> 
-                <ul class="nav navbar-nav bottomnav">
-                    <li><a href="/sorin/academics" class="hvr-underline-from-center">Academics/Curriculum</a></li>
-                    <li><a href="/sorin/athletics" class="hvr-underline-from-center">Athletics</a></li>
-                    <li><a href="/sorin/music" class="hvr-underline-from-center">Music</a></li>
-                    <li><a href="/sorin/arts-theatre" class="hvr-underline-from-center">Arts & Theatre</a></li>
-                    <li><a href="/sorin/activities" class="hvr-underline-from-center">Activities</a></li>
-                    <li><a href="/sorin/guidance" class="hvr-underline-from-center">Guidance</a></li>
-                    <li><a href="/sorin/about" class="hvr-underline-from-center">About</a></li>
-                </ul>
-            </nav>
-        </div>
+        <!-- search goes here -->
+    </ul>
+    <ul class="nav navbar-nav navbar-extra-resources">
+        <li class="nav-item"><a href="/sorin/directory">Directory</a></li>
+        <li class="nav-item"><a href="/sorin/map">Campus Map</a></li>
+    </ul>
+</nav>
+
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <a class="navbar-brand" href="/sorin/index.php" title="Mentor High School Homepage">
+        <img src="/sorin/inc/mentor.png" alt="Mentor High School heading">
+    </a> 
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#header-bottom-nav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="header-bottom-nav">
+        <ul class="nav navbar-nav ml-auto">
+            <li class="nav-item ml-auto mr-auto"><a href="/sorin/academics" class="nav-link hvr-underline-from-center">Academics/Curriculum</a></li>
+            <li class="nav-item ml-auto mr-auto"><a href="/sorin/athletics" class="nav-link hvr-underline-from-center">Athletics</a></li>
+            <li class="nav-item ml-auto mr-auto"><a href="/sorin/music" class="nav-link hvr-underline-from-center">Music</a></li>
+            <li class="nav-item ml-auto mr-auto"><a href="/sorin/arts-theatre" class="nav-link hvr-underline-from-center">Arts & Theatre</a></li>
+            <li class="nav-item ml-auto mr-auto"><a href="/sorin/activities" class="nav-link hvr-underline-from-center">Activities</a></li>
+            <li class="nav-item ml-auto mr-auto"><a href="/sorin/guidance" class="nav-link hvr-underline-from-center">Guidance</a></li>
+            <li class="nav-item ml-auto mr-auto"><a href="/sorin/about" class="nav-link hvr-underline-from-center">About</a></li>
+        </ul>
     </div>
+</nav>
