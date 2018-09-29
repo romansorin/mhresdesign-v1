388acf25e89397f8c490ce0f42b5d799fe5021cd
Ryan Rapini <ryanrapini@gmail.com>
2018-09-29 18:16:27 -0400

    remove padding

--- index.php
+++ index.php
@@ -11,23 +11,24 @@
 <body>
     <?php include 'inc/header.php' ?>
     <!-- This is your main container. -->
-    <div class="container-fluid">
-            <div class="col-12">
+    <div class="container-fluid no-padding">
+        <div class="row no-padding">
+            <div class="col-12 no-padding">
                 <div id="frontpage-carousel" class="carousel slide" data-ride="carousel">
                     <ol class="carousel-indicators">
                         <li data-target="#frontpage-carousel" data-slide-to="0" class="active"></li>
                         <li data-target="#frontpage-carousel" data-slide-to="1"></li>
                         <li data-target="#frontpage-carousel" data-slide-to="2"></li>
                     </ol>
-                    <div class="carousel-inner" style="height:400px">
+                    <div class="carousel-inner">
                         <div class="carousel-item active">
                             <img class="d-block w-100" src="images/1.jpg" alt="First slide">
 			</div>
 			<div class="carousel-item">
 				<img class="d-block w-100" src="images/2.jpg" alt="Second slide">
 			</div>
-			<div class="carousel-item w-100">
-				<img class="d-block" src="images/3.jpg" alt="Third slide">
+			<div class="carousel-item">
+				<img class="d-block w-100" src="images/3.jpg" alt="Third slide">
 			</div>
                     </div>
                     <a class="carousel-control-prev" href="#frontpage-carousel" role="button" data-slide="prev">
@@ -39,6 +40,7 @@
                         <span class="sr-only">Next</span>
                     </a>
                 </div>
+            </div>
         </div>
         <div class="row">
             <div class="col-12">
