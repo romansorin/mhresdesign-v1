08bf074a3422b15fb40b33abb70ff0d3103aac3d
Ryan Rapini <ryanrapini@gmail.com>
2018-09-29 17:34:59 -0400

    photos

--- index.php
+++ index.php
@@ -12,27 +12,23 @@
     <?php include 'inc/header.php' ?>
     <!-- This is your main container. -->
     <div class="container-fluid">
-        <div class="row">
-            <div class="col-4 ml-auto mr-auto">
+            <div class="col-12">
                 <div id="frontpage-carousel" class="carousel slide" data-ride="carousel">
                     <ol class="carousel-indicators">
                         <li data-target="#frontpage-carousel" data-slide-to="0" class="active"></li>
                         <li data-target="#frontpage-carousel" data-slide-to="1"></li>
                         <li data-target="#frontpage-carousel" data-slide-to="2"></li>
                     </ol>
-                    <div class="carousel-inner">
+                    <div class="carousel-inner" style="height:400px">
                         <div class="carousel-item active">
-                            <img class="d-block" src="images/sample.jpg" alt="First slide">
-                        </div>
-                        <div class="carousel-item">
-                            <img class="d-block" src="images/1.jpg" alt="First slide">
-                        
-<img class="d-block" src="images/2.jpg" alt="Second slide">
-<img class="d-block" src="images/3.jpg" alt="Third  slide">
-				</div>
-                        <div class="carousel-item">
-                            <img class="d-block" src="images/sample.jpg" alt="Third slide">
-                        </div>
+                            <img class="d-block w-100" src="images/1.jpg" alt="First slide">
+			</div>
+			<div class="carousel-item">
+				<img class="d-block w-100" src="images/2.jpg" alt="Second slide">
+			</div>
+			<div class="carousel-item w-100">
+				<img class="d-block" src="images/3.jpg" alt="Third slide">
+			</div>
                     </div>
                     <a class="carousel-control-prev" href="#frontpage-carousel" role="button" data-slide="prev">
                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -43,7 +39,6 @@
                         <span class="sr-only">Next</span>
                     </a>
                 </div>
-            </div>
         </div>
         <div class="row">
             <div class="col-12">
