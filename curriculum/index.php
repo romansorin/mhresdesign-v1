f681141f6cb845bb38e4c36a18363ea9303bef6f
Ryan Rapini <ryanrapini@gmail.com>
2018-09-29 16:53:53 -0400

    fixing the main page

---
+++ curriculum/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include '../inc/includes.php'; ?>
+    <title>Home | Mentor High School</title>
+    <meta name="description" content="This is a description">
+</head>
+
+<body>
+    <?php include '../inc/header.php' ?>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="main-photos col-md-12">
+                <img class="img-responsive" src="" alt="photos here">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+            </div>
+            <div class="col-md-8">
+            </div>
+        </div>
+    </div>
+    <?php include '../inc/footer.php'; ?>
+</body>
+
+</html>
