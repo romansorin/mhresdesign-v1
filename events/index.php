dd3678a4d3a98c3f93b008542238acc85e43a5ce
Roman Sorin <988244@students.mentorschools.org>
2018-10-07 18:55:48 -0400

    main-page body has been built
    lots of major changes structurally and graphically; header and footer will be re-done within the next week or two.

---
+++ events/index.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "empty index for events directory";
+
+?>
--- index.php
+++ index.php
@@ -23,10 +23,10 @@
                     </ol>
                     <div class="carousel-inner" style="height: 400px">
                         <div class="carousel-item active">
-                            <img class="d-block w-100" src="images/1.jpg" alt="First slide">
+                            <img class="d-block w-100" src="https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/GbbtDTW/new-york-city-nyc-sunset-night-skyline-view-brooklyn-bridge-water-4k-timelapse_41kywuhn__F0000.png" alt="First slide">
                         </div>
                         <div class="carousel-item">
-                            <img class="d-block w-100" src="images/2.jpg" alt="Second slide">
+                            <img class="d-block w-100" src="http://orig07.deviantart.net/bd5d/f/2014/334/d/c/new_york_skyline_wallpaper_by_neokeitaro-d8839i9.jpg" alt="Second slide">
                         </div>
                         <div class="carousel-item">
                             <img class="d-block w-100" src="images/3.jpg" alt="Third slide">
@@ -45,94 +45,147 @@
         </div>
     </div>
     <!-- This is the news section of the main page. -->
-    <div class="container-fluid no-padding">
-        <div class="row news-header">
-            <div class="col-sm-12">
-                <h2 id="news-header-label">Mentor News</h2>
-                <p id="news-header-desc">See what's happening at Mentor</p>
+    <div class="container-fluid no-padding news-container">
+        <div class="container no-padding">
+            <div class="row news-header">
+                <div class="col-sm-12">
+                    <h2 id="news-header-label">Mentor News</h2>
+                    <p id="news-header-desc">See what's happening at Mentor</p>
+                </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-4">
-                <article>
-                    <figure class="article-img"></figure>
-                    <div class="content">
-                        <div class="post-meta">
-                            <p class="post-category">Clubs</p>
+            <div class="row">
+                <div class="col-sm-4" style="padding-left: 0px; padding-right: 0px;">
+                    <article>
+                        <figure class="article-img">
+                            <img src="https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/GbbtDTW/new-york-city-nyc-sunset-night-skyline-view-brooklyn-bridge-water-4k-timelapse_41kywuhn__F0000.png" alt="test">
+                        </figure>
+                        <div class="content">
+                            <div class="post-meta">
+                                <p class="post-category">Category</p>
+                            </div>
+                            <h3>Corresponding category news title for one</h3>
                         </div>
-                        <h3>Main title of Clubs news</h3>
-                    </div>
-                </article>
-            </div>
-            <div class="col-sm-4">
-                <article>
-                    <figure class="article-img"></figure>
-                    <div class="content">
-                        <div class="post-meta">
-                            <p class="post-category">Sports</p>
+                    </article>
+                </div>
+                <div class="col-sm-4" style="padding-left: 0px; padding-right: 0px;">
+                    <article>
+                        <figure class="article-img">
+                            <img src="https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/GbbtDTW/new-york-city-nyc-sunset-night-skyline-view-brooklyn-bridge-water-4k-timelapse_41kywuhn__F0000.png" alt="test">
+                        </figure>
+                        <div class="content">
+                            <div class="post-meta">
+                                <p class="post-category">Category</p>
+                            </div>
+                            <h3>Corresponding category news title for two</h3>
                         </div>
-                        <h3>Main title of Sports news</h3>
-                    </div>
-                </article>
-            </div>
-            <div class="col-sm-4">
-                <article>
-                    <figure class="article-img"></figure>
-                    <div class="content">
-                        <div class="post-meta">
-                            <p class="post-category">Guidance</p>
+                    </article>
+                </div>
+                <div class="col-sm-4" style="padding-left: 0px; padding-right: 0px;">
+                    <article>
+                        <figure class="article-img">
+                            <img src="https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/GbbtDTW/new-york-city-nyc-sunset-night-skyline-view-brooklyn-bridge-water-4k-timelapse_41kywuhn__F0000.png" alt="test">
+                        </figure>
+                        <div class="content">
+                            <div class="post-meta">
+                                <p class="post-category">Category</p>
+                            </div>
+                            <h3>Corresponding category news title for three</h3>
                         </div>
-                        <h3>Main title of Guidance news</h3>
-                    </div>
-                </article>
+                    </article>
+                </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-8">
-                <article>
-                    <figure class="article-img"></figure>
-                    <div class="content">
-                        <div class="post-meta">
-                            <p class="post-category">Miscellaneous</p>
+            <div class="row">
+                <div class="col-sm-8" style="padding-left: 0px; padding-right: 0px;">
+                    <article class="article-long">
+                        <figure class="article-img">
+                            <img src="https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/GbbtDTW/new-york-city-nyc-sunset-night-skyline-view-brooklyn-bridge-water-4k-timelapse_41kywuhn__F0000.png" alt="test">
+                        </figure>
+                        <div class="content">
+                            <div class="post-meta">
+                                <p class="post-category">Featured Category</p>
+                            </div>
+                            <h3>Corresponding category news title for featured</h3>
                         </div>
-                        <h3>Main title of Misc. news</h3>
-                    </div>
-                </article>
-            </div>
-            <div class="col-sm-4">
-                <article>
-                    <figure class="article-img"></figure>
-                    <div class="content">
-                        <div class="post-meta">
-                            <p class="post-category">Important Announcements</p>
+                    </article>
+                </div>
+                <div class="col-sm-4" style="padding-left: 0px; padding-right: 0px;">
+                    <article>
+                        <figure class="article-img">
+                            <img src="https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/GbbtDTW/new-york-city-nyc-sunset-night-skyline-view-brooklyn-bridge-water-4k-timelapse_41kywuhn__F0000.png" alt="test">
+                        </figure>
+                        <div class="content">
+                            <div class="post-meta">
+                                <p class="post-category">Category</p>
+                            </div>
+                            <h3>Corresponding category news title for four</h3>
                         </div>
-                        <h3>Main title of Important Announcements</h3>
-                    </div>
-                </article>
+                    </article>
+                </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-12" style="text-align: center;">
-                <a href="/news"><button type="button" class="btn btn-light" id="all-news-btn">All Mentor News</button></a>
+            <div class="row">
+                <div class="col-sm-12" style="text-align: center;">
+                    <a href="/news"><button type="button" class="btn btn-light" id="all-news-btn">View all news</button></a>
+                </div>
             </div>
         </div>
     </div>
     <!-- This is the calendar of the main page. -->
-    <div class="container no-padding">
-        <div class="row calendar">
-            <?php include 'inc/calendar.php'; 
-            $calendar = new CalendarWidget;
-            $calendar->generate(4);
-            ?>
+    <div class="container-fluid no-padding events-container">
+        <div class="container no-padding">
+            <div class="row events-header">
+                <div class="col-sm-12">
+                    <h2 id="events-header-label">Mentor Events</h2>
+                    <p id="events-header-desc">Upcoming events and meetings</p>
+                </div>
+            </div>
+            <div class="row">
+                <?php include 'inc/calendar.php'; 
+                $calendar = new CalendarWidget;
+                $calendar->generate(4);
+                ?>
+            </div>
+        </div>  
+        <div class="row">
+            <div class="col-sm-12" style="text-align: center;">
+                <a href="/events"><button type="button" class="btn btn-light" id="all-events-btn">View all events</button></a>
+            </div>
         </div>
     </div>
     <!-- This is the social media section of the main page -->
-    <div class="container-fluid no-padding">
-        <div class="row twitter-feeds">
-           <!-- twitters -->
+    <div class="container no-padding">
+        <div class="row social-header">
+            <div class="col-sm-12">
+                <h2 id="social-header-label">Connect with us</h2>
+                <p id="social-header-desc">Stay updated with campus life</p>
+            </div>
+        </div>
+        <div class="row social-media">
+            <div class="col-sm-4 crowe-twitter">    
+                <a href="https://twitter.com/PrincipalCrowe" alt="Official account of MHS principal">
+                    <i class="fa fa-twitter"></i>
+                    <h4 class="sm-title">@PrincipalCrowe</h4>
+                </a>
+                <p class="tweet-content">Homecoming Assembly kicks off with our Alma Mater <a href="https://twitter.com/mentorhigh">@MentorHigh</a></p>
+                <h4 class="time-span">About 7 minutes ago</h4>
+            </div>
+            <div class="col-sm-4 instagram">
+                <a href="https://instagram.com/rm.sorin" alt="I made this website">
+                    <i class="fa fa-instagram"></i>
+                    <h4 class="sm-title">rm.sorin</h4>
+                    <a href="https://www.instagram.com/p/BmDycX-HLD-/?taken-by=rm.sorin" alt="test insta post"><img src="https://scontent-iad3-1.cdninstagram.com/vp/c2fbf259b3bb22bf664d9596d3f688e1/5C612A60/t51.2885-15/e35/38057971_512894979140557_5238749822009212928_n.jpg" alt="test photo" id="embed-ig"></a>
+                </a>
+            </div>
+            <div class="col-sm-4 general-twitter">
+                <a href="https://twitter.com/mentorhigh" alt="Official Mentor High School account">
+                    <i class="fa fa-twitter"></i>
+                    <h4 class="sm-title">@MentorHigh</h4>
+                </a>
+                <p class="tweet-content">RT <a href="https://twitter.com/mrglavanmhs">@MrGlavanMHS</a>: Thank you <a href="https://twitter.com/repdavejoyce">@RepDaveJoyce</a> for visiting the 2018 Lake County Think Manufacturing Expo and supporting our students and local businesses!</p>
+                <h4 class="time-span">About 12 hours ago</h4>
+            </div>
         </div>
     </div>
-<?php include 'inc/footer.php'; ?>
+    <?php include 'inc/footer.php'; ?>
 </body>
 
 </html>
