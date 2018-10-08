d04c18838d02c794d11a8138f18d465c67faff21
Roman Sorin <988244@students.mentorschools.org>
2018-10-08 11:37:55 -0400

    testweather dir

---
+++ testweather/README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/fleeting/pen/xklfq.
+
+ Full docs can be found at http://simpleweatherjs.com.
---
+++ testweather/js/index.js
@@ -0,0 +1,19 @@
+// v3.1.0
+//Docs at http://simpleweatherjs.com
+$(document).ready(function() {
+  $.simpleWeather({
+    location: 'Austin, TX',
+    woeid: '',
+    unit: 'f',
+    success: function(weather) {
+      html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+      html += '<li class="currently">'+weather.currently+'</li>';
+  
+      $("#weather").html(html);
+    },
+    error: function(error) {
+      $("#weather").html('<p>'+error+'</p>');
+    }
+  });
+});
