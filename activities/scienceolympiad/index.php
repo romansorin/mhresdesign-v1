8e1936a165d30dc9593a2aaef4e1e315fb6cc706
Roman Sorin <988244@students.mentorschools.org>
2018-10-09 12:20:28 -0400

    re-create index.php sci-oly

---
+++ activities/scienceolympiad/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include '../inc/includes.php'; ?>
+    <title>Science Olympiad | Mentor High School</title>
+    <meta name="description" content="This is a description">
+</head>
+
+<body>
+    <?php include '../inc/header.php' ?>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="main-photos col-md-12">
+                <img class="img-responsive" src="" alt="photos here">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+            	<p>empty dir for sci-oly
+            		<br>
+            		picture
+            		<br><br>
+					"blurb" of text about science olympiad
+					<br>
+					information about logan leak and the university of chicago
+					<br>
+					2017 champs
+				</p>
+            </div>
+        </div>
+    </div>
+    <?php include '../inc/footer.php'; ?>
+</body>
+
+</html>
