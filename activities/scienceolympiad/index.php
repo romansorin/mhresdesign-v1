4052bb1fc0314b991604afed140d9227e4da9afb
romansorin <rmaximsorin@gmail.com>
2018-10-09 12:11:03 -0400

    more sci-oly index info

--- activities/scienceolympiad/index.php
+++ activities/scienceolympiad/index.php
@@ -1,5 +1,9 @@
 <?php 
 
-echo "empty dir for sci-oly";
+echo "empty dir for sci-oly<br>";
+echo "picture<br><br>";
+echo "\"blurb\" of text about science olympiad<br>";
+echo "information about logan leak and the university of chicago<br>";
+echo "2017 champs";
 
 ?>
