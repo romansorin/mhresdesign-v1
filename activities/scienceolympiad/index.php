fa2c77deed5f0a790f1bd865c4aff12dd193d905
Roman Sorin <988244@students.mentorschools.org>
2018-10-09 12:14:10 -0400

    sci-oly filler info

--- activities/scienceolympiad/index.php
+++ activities/scienceolympiad/index.php
@@ -1,9 +1,36 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
 
-echo "empty dir for sci-oly<br>";
-echo "picture<br><br>";
-echo "\"blurb\" of text about science olympiad<br>";
-echo "information about logan leak and the university of chicago<br>";
-echo "2017 champs";
+<head>
+    <?php include '../inc/includes.php'; ?>
+    <title>Science Olympiad | Mentor High School</title>
+    <meta name="description" content="This is a description">
+</head>
 
-?>
+<body>
+    <?php include '../inc/header.php' ?>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="main-photos col-md-12">
+                <img class="img-responsive" src="" alt="photos here">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+            	<p>empty dir for sci-oly
+            		<br>
+            		picture
+            		<br><br>
+					"blurb" of text about science olympiad
+					<br>
+					information about logan leak and the university of chicago
+					<br>
+					2017 champs
+				</p>
+            </div>
+        </div>
+    </div>
+    <?php include '../inc/footer.php'; ?>
+</body>
+
+</html>
