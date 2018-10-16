af0ee17a9ace9241d384c01db96ca52a22bb67e9
romansorin <rmaximsorin@gmail.com>
2018-10-15 22:13:04 -0400

    fixed issue with weather widget not displaying sitewide

--- inc/includes.php
+++ inc/includes.php
@@ -16,3 +16,5 @@
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="/js/bootstrap.bundle.min.js"></script>
+
+    <script src="/js/weather.js"></script>
