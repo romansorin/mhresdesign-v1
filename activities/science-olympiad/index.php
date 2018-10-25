02e9c3f0306776f198b553d3a1ea9ee56c007a95
Roman Sorin <988244@students.mentorschools.org>
2018-10-25 19:24:08 -0400

    barebones scioly

--- activities/science-olympiad/index.php
+++ activities/science-olympiad/index.php
@@ -10,12 +10,42 @@
 <body>
     <div class="push-down"></div>
     <?php include '/srv/http/inc/header.php' ?>
-    <div class="container-fluid">
+    <div class="container-fluid no-padding">
         <div class="row">
             <div class="main-photos col-md-12">
-                <img class="img-responsive" src="" alt="photos here">
+                <img class="img-responsive" src="#" alt="Photos will go here">
             </div>
         </div>
+    </div>
+    <div class="container no-padding">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Science Olympiad</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur tristique diam, ac mattis diam cursus quis. Aenean cursus sollicitudin condimentum. Duis ac luctus quam. Curabitur id sem pharetra, sollicitudin ante ac, dapibus sem. Nullam elementum volutpat felis et cursus. Nam pellentesque sit amet sapien at malesuada. Suspendisse potenti. Sed luctus condimentum urna ac imperdiet. Ut facilisis sem in sapien hendrerit, eu hendrerit tellus convallis. Curabitur pulvinar eros a turpis accumsan rutrum. Sed et lacus dui.</p>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid no-padding">
+        <div class="container no-padding">
+            <div class="row"></div>
+        </div>
+    </div>
+    <div class="container-fluid no-padding">
+        <div class="container no-padding">
+            <div class="row"></div>
+        </div>
+    </div>
+    <div class="container no-padding">
+        <div class="row">
+            <div class="col-md-6"></div>
+            <div class="col-md-6"></div>
+        </div>
+    </div>
+    <!--
         <div class="row">
             <div class="col-md-12">
             	<p>empty dir for sci-oly
@@ -37,6 +67,8 @@
             </div>
         </div>
     </div>
+
+-->
     <?php include '/srv/http/inc/footer.php'; ?>
 </body>
 
