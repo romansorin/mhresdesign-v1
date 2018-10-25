fa8a9cc5eda7e9799ced7b236c0260955ba36733
Roman Sorin <988244@students.mentorschools.org>
2018-10-25 07:33:23 -0400

    creation of some academics subdirs, css file changes (global, mainpage)

---
+++ academics/health-pe/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include '/srv/http/inc/includes.php'; ?>
+    <title>Health & Physical Education | Mentor High School</title>
+    <meta name="description" content="This is a description">
+</head>
+
+<body>
+    <div class="push-down"></div>
+    <?php include '/srv/http/inc/header.php' ?>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="main-photos col-md-12">
+                <img class="img-responsive" src="" alt="photos here">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Health & Physical Education</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ullamcorper, libero quis interdum convallis, orci sapien imperdiet erat, rutrum malesuada elit libero et lectus. Aenean vitae pellentesque ligula. Nullam a purus malesuada, gravida tellus sit amet, fringilla dui. Duis congue diam turpis, et consequat erat iaculis vitae. Nulla at auctor orci, in aliquam nisi. Suspendisse eu convallis nunc, vitae feugiat orci. Nullam faucibus ipsum nec nisl maximus tempus. Etiam vitae velit sit amet lacus sagittis condimentum sit amet sed ante. Morbi blandit lacus sit amet turpis aliquet accumsan. Donec auctor condimentum neque, sed maximus diam egestas in. Nulla viverra commodo metus vel aliquet. Maecenas euismod justo sed elit porta, non faucibus enim hendrerit. Ut laoreet diam eu rhoncus cursus. Suspendisse lacinia ut eros ut rhoncus. In hac habitasse platea dictumst. Sed volutpat mi purus, non ultricies arcu vestibulum id.</p>
+                <p>Suspendisse eget porta quam, nec mollis augue. Mauris volutpat facilisis lectus eu porttitor. Aenean ut urna odio. Mauris consectetur, urna in laoreet blandit, turpis tortor feugiat lorem, at interdum nisl nulla ut lectus. Sed vulputate, ligula eu aliquet scelerisque, ex justo molestie quam, at imperdiet neque dui in urna. Integer tincidunt varius lacus quis lobortis. Donec vulputate hendrerit dictum. In ut tortor elementum, suscipit lectus vitae, condimentum dolor. Vivamus varius fringilla elementum. Sed aliquam scelerisque est id vehicula. Quisque feugiat dictum mollis. Suspendisse eget lobortis neque, malesuada placerat neque. Duis ut interdum justo. Ut aliquet pellentesque diam in posuere. Fusce non metus egestas massa bibendum bibendum in vel odio. Mauris in purus arcu.</p>
+                <p>Nam tincidunt, leo nec tincidunt semper, lacus enim cursus ipsum, molestie posuere metus elit vitae ipsum. Aenean finibus nunc lorem, quis elementum felis lacinia sodales. Nam imperdiet at leo non sollicitudin. Donec arcu odio, elementum vitae faucibus luctus, mattis tristique dui. Pellentesque feugiat sem ultricies ultricies blandit. Etiam commodo diam a vehicula aliquam. In egestas felis risus, ut varius mauris gravida in. Integer ultricies eros quis ultrices finibus. Nulla ut neque quis libero rutrum molestie nec at augue. Donec pharetra enim sit amet dolor semper rhoncus. Sed sagittis rutrum orci non condimentum. Nullam iaculis purus sed elit facilisis ultricies. Mauris interdum gravida tempor. Pellentesque vel elementum turpis. Maecenas vel ultricies purus. Donec et accumsan odio.</p>
+                <p>Donec ullamcorper lobortis tortor, ornare condimentum mi accumsan non. Vestibulum sollicitudin porta est a auctor. In hac habitasse platea dictumst. Vivamus dictum tellus imperdiet lorem dictum iaculis. Vestibulum vitae mauris lacus. Ut sollicitudin rhoncus nisi at pharetra. Curabitur sem ante, mollis eu augue eu, molestie tristique tortor. Fusce consectetur, ante ut auctor imperdiet, mi magna auctor nunc, vitae pharetra orci dui ac sem. Vivamus placerat enim nibh, eu faucibus nulla rutrum quis. Mauris tincidunt massa et elit consequat tempus. Sed sit amet nunc imperdiet, facilisis lorem a, ultricies massa. Nulla ut ullamcorper quam, vitae lacinia tortor. Phasellus eleifend, nisl sit amet semper facilisis, quam est imperdiet odio, eu sollicitudin purus nisi quis odio.</p>
+                <p>Donec ullamcorper lobortis tortor, ornare condimentum mi accumsan non. Vestibulum sollicitudin porta est a auctor. In hac habitasse platea dictumst. Vivamus dictum tellus imperdiet lorem dictum iaculis. Vestibulum vitae mauris lacus. Ut sollicitudin rhoncus nisi at pharetra. Curabitur sem ante, mollis eu augue eu, molestie tristique tortor. Fusce consectetur, ante ut auctor imperdiet, mi magna auctor nunc, vitae pharetra orci dui ac sem. Vivamus placerat enim nibh, eu faucibus nulla rutrum quis. Mauris tincidunt massa et elit consequat tempus. Sed sit amet nunc imperdiet, facilisis lorem a, ultricies massa. Nulla ut ullamcorper quam, vitae lacinia tortor. Phasellus eleifend, nisl sit amet semper facilisis, quam est imperdiet odio, eu sollicitudin purus nisi quis odio.</p>
+                <p>Donec ullamcorper lobortis tortor, ornare condimentum mi accumsan non. Vestibulum sollicitudin porta est a auctor. In hac habitasse platea dictumst. Vivamus dictum tellus imperdiet lorem dictum iaculis. Vestibulum vitae mauris lacus. Ut sollicitudin rhoncus nisi at pharetra. Curabitur sem ante, mollis eu augue eu, molestie tristique tortor. Fusce consectetur, ante ut auctor imperdiet, mi magna auctor nunc, vitae pharetra orci dui ac sem. Vivamus placerat enim nibh, eu faucibus nulla rutrum quis. Mauris tincidunt massa et elit consequat tempus. Sed sit amet nunc imperdiet, facilisis lorem a, ultricies massa. Nulla ut ullamcorper quam, vitae lacinia tortor. Phasellus eleifend, nisl sit amet semper facilisis, quam est imperdiet odio, eu sollicitudin purus nisi quis odio.</p>
+                <p>Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam ac nulla a dolor condimentum fringilla id ut magna. In rhoncus mi nisi, nec interdum nisi finibus in. Donec et accumsan lectus. Nulla posuere ornare dui, ac pretium tortor consequat eu. Nunc eget justo sed magna imperdiet porta in sit amet nisi. Nullam lobortis, justo ac congue posuere, sapien lorem rhoncus magna, nec condimentum turpis libero in diam.</p>
+            </div>
+        </div>
+    </div>
+    <?php include '/srv/http/inc/footer.php'; ?>
+</body>
+
+</html>
--- academics/index.php
+++ academics/index.php
@@ -13,8 +13,24 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-12">
-               <h1>Academics</h1>
-               <h3>Explore the variety of programs and courses offered at Mentor High School.</h3>
+                <h1>Academics</h1>
+                <h3>Explore the variety of programs and courses offered at Mentor High School.</h3>
+
+                <h6>Business/Computer Science</h6>
+                <h6>Career/Technical Education</h6>
+                <h6>English/Language Arts</h6>
+                <h6>Family and Consumer Sciences</h6>
+                <h6>Health and Physical Education</h6>
+                <h6>Mathematics</h6>
+                <h6>Science</h6>
+                <h6>Social Studies</h6>
+                <h6>Technology Education</h6>
+                <h6>World Language</h6>
+                <h6>Online Courses</h6>
+                <h6>Distance Learning Electives</h6>
+                <h6>Blended Learning Elective</h6>
+                <h6>Required Online Courses For All Students</h6>
+                <h6>Lakeland Dual Credit Courses</h6>
             </div>
         </div>
     </div>
