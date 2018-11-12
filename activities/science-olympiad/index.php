067b361cebbaa977e4887f90cf07ece634c66549
Roman Sorin <988244@students.mentorschools.org>
2018-11-11 21:58:00 -0500

    template for activities

--- activities/science-olympiad/index.php
+++ activities/science-olympiad/index.php
@@ -5,7 +5,7 @@
     <?php include '/srv/http/inc/includes.php'; ?>
     <link rel="stylesheet" href="/css/activities.css">
     <title>Science Olympiad | Mentor High School</title>
-    <meta name="description" content="This is a description">
+    <meta name="description" content="#">
 </head>
 
 <body>
@@ -14,7 +14,7 @@
     <div class="container-fluid no-padding">
         <div class="row">
             <div class="col-md-12 no-padding img-top">
-                <img class="d-block w-100 img-fit" src="https://wallpapercave.com/wp/W884Eoi.jpg" alt="Photos will go here">
+                <img class="d-block w-100 img-fit" src="https://wallpapercave.com/wp/W884Eoi.jpg" alt="Science Olympiad header photo">
                 <div class="img-text-overlay">
                     <a href="/activities"><h6>Activities</h6></a>
                     <h1>Science Olympiad</h1>
@@ -38,15 +38,6 @@
                                 </div>
                             </div>
                             <div class="dropdown">
-                                <script>
-                                    jQuery('div.dropdown').hover(function() {
-                                        jQuery(this).find('.local-nav-menu-item').addClass('active');
-                                        jQuery(this).find('.dropdown-content').stop(true, true).delay(200).fadeIn(500);
-                                    }, function() {
-                                        jQuery(this).find('.local-nav-menu-item').removeClass('active');
-                                        jQuery(this).find('.dropdown-content').stop(true, true).delay(200).fadeOut(500);
-                                    });
-                                </script>
                                 <li class="local-nav-menu-item hvr-underline-from-center">About</li>
                                 <div class="dropdown-content">
                                     <a href="https://www.soinc.org/sites/default/files/uploaded_files/2019_SO_Events.pdf">2019 Events</a>
@@ -56,6 +47,15 @@
                             <a href="https://goo.gl/forms/2UZSs54yBig7dVgv1"><li class="local-nav-menu-item hvr-underline-from-center">Invitational</li></a>
                             <a href="https://example.com"><li class="local-nav-menu-item hvr-underline-from-center">Booster</li></a>
                             <a href="https://twitter.com/sciolycpts?lang=en&lang=en"><li class="local-nav-menu-item hvr-underline-from-center">Twitter</li></a>
+                            <script>
+                                jQuery('div.dropdown').hover(function() {
+                                    jQuery(this).find('.local-nav-menu-item').addClass('active');
+                                    jQuery(this).find('.dropdown-content').stop(true, true).delay(200).fadeIn(500);
+                                }, function() {
+                                    jQuery(this).find('.local-nav-menu-item').removeClass('active');
+                                    jQuery(this).find('.dropdown-content').stop(true, true).delay(200).fadeOut(500);
+                                });
+                            </script>
                         </ul>
                     </div>
                 </div>
