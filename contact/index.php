11fe497c2464db0e7cc9de7b153be5fa1a259669
Roman Sorin <988244@students.mentorschools.org>
2018-11-23 12:14:59 -0500

    barebones contact page

--- contact/index.php
+++ contact/index.php
@@ -2,38 +2,56 @@
 <html lang="en">
 
 <head>
-    <?php include '/srv/http/inc/includes.php'; ?>
+    <?php include 'C:\Users\Roman\Documents\sorin/inc/includes.php'; ?>
+    <link rel="stylesheet" href="contact.css">
     <title>Contact Us | Mentor High School</title>
     <meta name="description" content="This is a description">
 </head>
 
 <body>
-    <div class="push-down"></div>
-    <?php include '/srv/http/inc/header.php' ?>
-    <div class="container-fluid">
+    <!-- <div class="push-down"></div> -->
+    <?php include 'C:\Users\Roman\Documents\sorin/inc/header.php' ?>
+    <div class="container">
+        <div id="contact-header"><h1>Contact Mentor</h1></div>
         <div class="row">
-            <div class="col-md-6">
-               <h1>Questions, comments, or concerns?</h1>
-               <h3>General information:</h3>
-               <p>Main Phone:</p>
-               <p>Address:</p>
-               <p>Hours of Operation:</p>
+            <div class="col-md-6 info">
+                <h2>General Information</h2>
+                <p><em>Main Address:</em>
+                    <br>
+                    6477 Center Street
+                    <br>
+                    Mentor, OH 44060
+                    <br>
+                    <strong>Tel:</strong> (440) 974-5300
+                </p>
+                <h3>Guidance</h3>
+                <p><strong>Tel:</strong> (440) 974-5330</p>
+                <h3>Security and safety</h3>
+                <p>Resource Officers, MPD<br>
+                    <strong>Tel:</strong> (440) 255-2818<br><br>
+                    Security<br>
+                    <strong>Tel:</strong> (440) 255-2597<br><br>
+                    <a id="link" href="https://cityofmentor.com/departments/police/">Mentor Police Department</a><br>
+                    <strong>Tel:</strong> (440) 255-1234 (non-emergency)
+                </p>
             </div>
-            <div class="col-md-6">
-                <h3>For staff specific contact information, please reference our <a href="/directory">directory</a>.</h3>
+            <div class="col-md-6 info">
+                <h3>For specific staff contact information, please reference our <a href="/directory">directory</a>.</h3>
+                <h3>Hours of operation:</h3>
+                <ul>
+                    <li>Monday:</li>
+                    <li>Tuesday:</li>
+                    <li>Wednesday:</li>
+                    <li>Thursday:</li>
+                    <li>Friday:</li>
+                </ul>
+                <h3>Get in touch</h3>
+                <p>Questions, comments, or concerns?</p>
+                <a id="contact-btn" class="btn btn-primary" href="contact.php" role="button">Let us know</a>
             </div>
         </div>
     </div>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-12">
-                <h1>Experiencing problems?</h1>
-                <h3>We're more than happy to help.</h3>
-            </div>
-        </div>
-        <p>The Mentor High website is managed by the MHS web team.</p>
-    </div>
-    <?php include '/srv/http/inc/footer.php'; ?>
+    <?php include 'C:\Users\Roman\Documents\sorin/inc/footer.php'; ?>
 </body>
 
 </html>
