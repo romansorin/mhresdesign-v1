976fe0e2dff5121459a34a20bba703bc40a0c5fb
Roman Sorin <988244@students.mentorschools.org>
2018-11-26 15:26:38 -0500

    contact form ver.1

---
+++ contact/form_process.php
@@ -0,0 +1,66 @@
+<?php
+require '/srv/http/inc/math_captcha.php'; /* Require statement will halt execution if the file cannot be found or used */
+
+/* Initialize these variables as empty strings */
+$name = $email = $subject = $message = $success = "";
+$name_error = $email_error = $message_error = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	/* These statements will check each field of the form for input and invalid characters */
+	if (empty($_POST["name"])) {
+		$name_error = "Name is required";
+	} else {
+		$name = test_input($_POST["name"]);
+		// check if name only contains letters and whitespace
+		if (!preg_match("/^[a-zA-Z ]*$/", $name)) {
+			$name_error = "Only letters and white space allowed";
+		}
+	}
+
+	if (empty($_POST["email"])) {
+		$email_error = "Email is required";
+	} else {
+		$email = test_input($_POST["email"]);
+		// check if e-mail address is the correct format
+		if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+			$email_error = "Invalid email format";
+		}
+	}
+
+	if (empty($_POST["subject"])) {
+		$subject = "Contact form submission"; /* When the user doesn't provide a subject, automatically name the subject */
+	} else {
+		$subject = $_POST["subject"]; /* Otherwise, keep the subject as initially defined */
+	}
+
+	if (empty($_POST["message"])) {
+		$message_error = "Message is required";
+	}
+
+	/* If no errors are present, set the content of the actual message */
+	if ($name_error == "" and $email_error == "" and $message_error == "" and $answer_error == "") {
+		$message_body = '';
+		unset($_POST['submit']);
+		foreach ($_POST as $key => $value) {
+			$message_body .= "$key: $value\n";
+		}
+
+		$to = "rmaximsorin@gmail.com"; /* Recipient of the e-mails */
+
+		/* If the parameters are valid, send the message */
+		if (mail($to, $subject, $message)) {
+			$success = "Message sent.";
+			$name = $email = $subject = $message = ''; /* Reset the values of the form */
+		}
+	}
+}
+
+/* Cleans up the data for security and parsing purposes */
+function test_input($data) {
+	$data = trim($data);
+	$data = stripslashes($data);
+	$data = htmlspecialchars($data);
+	return $data;
+}
+
+?>
