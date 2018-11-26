9b2fc7a8e0956e98eae1aa354daf85ca8c785120
Roman Sorin <988244@students.mentorschools.org>
2018-11-26 14:25:07 -0500

    simple math captcha (php)

---
+++ inc/math_captcha.php
@@ -0,0 +1,26 @@
+<?php
+/* Simple math captcha to stop more basic spam */
+
+$answer_error = "";
+$answer = "";
+
+$intA = random_int(0,9);
+$intB = random_int(0,9);
+
+$equation = $intA . ' + ' . $intB; /* This is used solely for display purposes */
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+	$expected_answer = $_POST["intA"] + $_POST["intB"]; /* Store the correct answer to the math problem in $_POST variables */
+
+	if (isset($_POST["answer"])) {
+		if ($_POST["answer"] == $expected_answer) {
+			$answer_error = "";
+		} elseif (empty($_POST["answer"])) {
+			$answer_error = "Answer is required";
+		} else {
+			$answer_error = "The answer you entered was incorrect.";
+		}
+	}
+}
+?>
