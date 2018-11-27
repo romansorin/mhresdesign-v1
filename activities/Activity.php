49131f4ffbe5b230efeb677d437c0e3a4fe16238
Roman Sorin <988244@students.mentorschools.org>
2018-11-26 21:17:21 -0500

    create activity file for class and methods

---
+++ activities/Activity.php
@@ -0,0 +1,15 @@
+<?php
+
+class Activity {
+	public $information;
+}
+
+function fetchActivityInformation($pdo, $activity) {
+	$info = $pdo->prepare("SELECT information FROM activities WHERE activity= '" . $activity . "'");
+
+	$info->execute();
+
+	return  $info->fetchAll(PDO::FETCH_CLASS, 'Activity');
+}
+
+?>
