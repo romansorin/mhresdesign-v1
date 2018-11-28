e730cdf09d1a47f1e6c735172330b0a056bc0221
Roman Sorin <988244@students.mentorschools.org>
2018-11-28 12:11:28 -0500

    asda

--- inc/math_captcha.php
+++ inc/math_captcha.php
@@ -1,6 +1,6 @@
 <?php
 /* Simple math captcha to stop basic spam */
-/*
+
 $answer_error = "";
 $answer       = "";
 
@@ -8,11 +8,11 @@ $intA = random_int(0, 9);
 $intB = random_int(0, 9);
 
 $equation = $intA . ' + ' . $intB; /* This is used solely for display purposes */
-/*
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $expected_answer = $_POST["intA"] + $_POST["intB"]; /* Store the correct answer to the math problem in $_POST variables */
-/*
+
     if (isset($_POST["answer"])) {
         if ($_POST["answer"] == $expected_answer) {
             $answer_error = "";
@@ -23,5 +23,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
     }
 }
-*/
+
 ?>
