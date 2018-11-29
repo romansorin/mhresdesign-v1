26a4912b3f0769356f7d1f62e3f524af9c2f6af8
Roman Sorin <988244@students.mentorschools.org>
2018-11-29 11:42:52 -0500

    sadad

--- directory/form_process.php
+++ directory/form_process.php
@@ -65,9 +65,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if (strlen($fac_staff) > 1) {
             $fs_error = "Only input a single character.";
         }
-        if (!($fac_staff === "F" || "S")) {
-            $fs_error = "You can only type F (faculty) or S (staff).";
-        }
     }
 
     if (empty($_POST["dept"])) {
