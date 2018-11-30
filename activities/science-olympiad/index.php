5ce24305c838dd6fee4f026f091eaf7dcf989d41
Roman Sorin <988244@students.mentorschools.org>
2018-11-30 13:41:45 -0500

    Error reporting

--- activities/science-olympiad/index.php
+++ activities/science-olympiad/index.php
@@ -15,11 +15,6 @@
 <body>
     <?php
 
-    
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
     /** @var Connection [Create a connection object] */
     $conn = new Connection();
 
--- index.php
+++ index.php
@@ -14,6 +14,11 @@
 
 <body>
     <?php
+
+    ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
     $conn = new Connection();
 
     $main = new MainPage();
