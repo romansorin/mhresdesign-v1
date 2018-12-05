f8146b35d2eb94c2feaaede74935dd788a12fdab
Roman Sorin <988244@students.mentorschools.org>
2018-12-05 07:19:53 -0500

    Changed button disabled attribute

--- inc/calendar.php
+++ inc/calendar.php
@@ -102,7 +102,7 @@ class CalendarWidget {
             } else {
                 echo "<br>";
                 echo "<div class\"col-sm-2 text-center no-padding\">";
-                echo "<button type=\"button\" class=\"btn event-link-btn-disabled disabled\">View</button>";
+                echo "<button type=\"button\" class=\"btn event-link-btn-disabled\" disabled>View</button>";
             }
             echo "</div>";
         }
