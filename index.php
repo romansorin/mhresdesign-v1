0bac47cdaa5e0dc7f25f9e2f8550575a7dc629ff
Roman Sorin <988244@students.mentorschools.org>
2018-12-04 23:31:39 -0500

    test function cont. index

--- index.php
+++ index.php
@@ -171,7 +171,7 @@ foreach ($results_image as $header_image): ?>
             <div class="row">
                 <?php require 'inc/calendar.php';
 $calendar = new CalendarWidget;
-$calendar->generate(4);
+$calendar->test;
 ?>
             </div>
         </div>
