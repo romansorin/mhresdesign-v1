68cd69d40fd493274737945daba9d964ce6c6640
Roman Sorin <988244@students.mentorschools.org>
2018-12-16 22:58:33 -0500

    hotjar only for main page

--- index.php
+++ index.php
@@ -13,6 +13,17 @@ require_once 'inc/TwitterAPI/config.php';
     <link rel="stylesheet" href="css/mainpage.css">
     <title>Mentor High School</title>
     <meta name="description" content="This is a description">
+        <!-- Hotjar Tracking Code for dev.mentorhigh.com -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:1130481,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
 </head>
 
 <body>
