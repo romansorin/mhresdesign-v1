a0ab481ebaa52c30f831d7696b514d64e1f362a9
Roman Sorin <988244@students.mentorschools.org>
2018-12-17 09:13:01 -0500

    Google Analytics tracking code

--- inc/includes.php
+++ inc/includes.php
@@ -31,3 +31,13 @@
         a.appendChild(r);
     })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
 </script>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-131028738-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-131028738-1');
+</script>
