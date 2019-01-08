be5dc23c2919ed6b0bee0612073fa43a25bb45ba
Roman Sorin <988244@students.mentorschools.org>
2019-01-07 23:15:51 -0500

    Created basic representation of dashboard mockup

--- postsystemp/index.php
+++ postsystemp/index.php
@@ -5,49 +5,71 @@
 	<meta name="description" content="">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Dashboard</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+	<?php include '../inc/includes.php';?>
+	<link rel="stylesheet" href="dashboard.css">
 </head>
 
-<style>
-.sidebar {
-	background: #2a3542;
-	position: fixed;
-	height: 100%;
-}
-#sidebar-expanded {
-	display: block;
-	width: 210px;
-	
-	
-	
-}
-
-#main-content {
-	margin-left: 210px;
-}
-
-
-#sidebar-collapsed {
-	display: none;
-	width: 55px;
-}
-
-</style>
-
 <body>
-	<div class="container-fluid">
+
+	<nav class="navbar navbar-expand-lg" id="top-nav">
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<div class="navbar-nav ml-auto">
+				<div class="nav-item border-divider"></div>
+				<a class="nav-link" href="#" id="usernameDropdownToggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Username<span class="caret-dropdown"></span></button>
+				</a>
+				<div id="usernameDropdown" class="dropdown-menu dropdown-menu-right" aria-labelledby="usernameDropdown">
+					<span class="dropdown-item-text" href="#"><strong>Status:</strong> Online</span>
+					<div class="dropdown-divider"></div>
+					<a class="dropdown-item" href="#">Settings</a>
+					<div class="dropdown-divider"></div>
+					<a class="dropdown-item" href="#">Feedback</a>
+					<div class="dropdown-divider"></div>
+					<!--<a class="dropdown-item" href="#">Lock</a>
+						<div class="dropdown-divider"></div>-->
+						<a class="dropdown-item" href="logout.inc.php">Logout</a>
+					</div>
+				</div>
+			</div>
+		</nav>
+
+		<div class="sidebar" id="sidebar-expanded">
+			<div class="row">
+				<div class="brand">
+					<div class="col-8">
+						<a class="navbar-brand" href="#">Dashboard</a>
+					</div>
+					<div class="col-4">
+						<i class="fa fa-reorder toggler"></i>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<nav class="nav nav-tabs flex-column" id="tabs" role="tablist">
+					<a class="nav-link" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="false">Home</a>
+					<a class="nav-link active" id="dashboard-tab" data-toggle="tab" href="#dashboard" role="tab" aria-controls="dashboard" aria-selected="true">Dashboard</a>
+					<a class="nav-link" id="pages-tab" data-toggle="tab" href="#pages" role="tab" aria-controls="pages" aria-selected="false">Pages</a>
+				</nav>
+			</div>
+		</div>
+
+		<div class="sidebar" id="sidebar-collapsed">
+			<i class="fa fa-reorder toggler"></i>
+			<nav class="nav nav-tabs flex-column">
+				<a class="navbar-brand" href="#">N</a>
+				<a class="nav-link active" href="#">A</a>
+				<a class="nav-link" href="#">Link</a>
+				<a class="nav-link" href="#">Link</a>
+				<a class="nav-link disabled" href="#">Disabled</a>
+			</nav>
+		</div>
 
 
-		<div class="navbar">toggle icon in here</div>
-		<div class="sidebar" id="sidebar-expanded">sidebar content here</div>
-		<div class="sidebar" id="sidebar-collapsed">hi</div>
-		<section id="main-content">main page content here</section>
-	</div>
 
-	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
-	<script src="main.js"></script>
+		<script src="main.js"></script>
 
-</body>
-</html>
+	</body>
+	</html>
--- postsystemp/main.js
+++ postsystemp/main.js
@@ -1,4 +1,17 @@
-$('.navbar').on('click', function(){
-    $('#sidebar-expanded').toggle();
-    $('#sidebar-collapsed').toggle();
+$(document).ready(function() {
+    $('.toggler').click(function() {
+        let expanded = $('#sidebar-expanded');
+        let collapsed = $('#sidebar-collapsed');
+        if (expanded.is(":visible")) {
+            expanded.hide();
+            collapsed.show();
+        } else if (collapsed.is(":visible")) {
+            collapsed.hide();
+            expanded.show();
+        }
+    });
+    $('#usernameDropdownToggle').click(function(e) {
+        e.preventDefault();
+        $('#usernameDropdownToggle .caret-dropdown').toggleClass('active');
+    });
 });
