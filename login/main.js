5e7b9c0cc95566ea23a7b4d4c8e6ffb165589e6a
Roman Sorin <988244@students.mentorschools.org>
2019-01-13 16:12:28 -0500

    Added password reset idea

--- login/main.js
+++ login/main.js
@@ -1,14 +1,17 @@
-$(document).ready(function() {
-    $('#signup-redirect-ref').click(function() {
-        $('.content').hide();
-        $('#signup').fadeIn("slow");
-    });
-    $('#signin-redirect-ref').click(function() {
-        $('.content').hide();
-        $('#login').fadeIn("slow");
-    });
-    $('#forgotPassword').click(function() {
-        $('.content').hide();
-        $('#forgot-password').fadeIn("slow");
-    });
+$('#signup-redirect-ref').click(function() {
+    $('.content').hide();
+    $('#signup').fadeIn("slow");
+});
+$('.signin-redirect-ref').click(function() {
+    $('.content').hide();
+    $('#login').fadeIn("slow");
+});
+$('#forgotPassword').click(function() {
+    $('.content').hide();
+    $('#forgot-password').fadeIn("slow");
+});
+$('#password-reset-submit').click(function(e) {
+    e.preventDefault();
+    let info = document.getElementById('info');
+    info.innerHTML = 'test!';
 });
