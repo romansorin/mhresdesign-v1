7d3123f75d6d2baca7ae4ef85cb3027c3063e368
Roman Sorin <988244@students.mentorschools.org>
2019-01-13 16:13:17 -0500

    Setting up password-reset frontend

--- login/index.php
+++ login/index.php
@@ -3,32 +3,33 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
-	<?php include '../inc/includes.php'; ?>
+	<?php include '../inc/includes.php';?>
 	<link rel="stylesheet" href="/css/login.css">
 </head>
 <body>
 
-		<div class="container-fluid">
-			<div class="row">
-				<div class="col-md-7 no-padding bg-img-col">
-					<img src="https://images.pexels.com/photos/160107/pexels-photo-160107.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="code background" id="bg-img">
-				</div>
-				<div class="col-md-5">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-7 no-padding bg-img-col">
+				<img src="https://images.pexels.com/photos/160107/pexels-photo-160107.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="code background" id="bg-img">
+			</div>
+			<div class="col-md-5">
 				<div class="content" id="login">
 					<div class="heading-wrapper">
 						<h1>Welcome</h1>
 						<h2>Welcome back! Please login to your account.</h2>
 					</div>
 					<div class="form-wrapper">
-						<form action="#" method="post">
+						<form action="#" method="post" id="signin-form">
 							<div class="form-group">
-								<input type="text" class="form-inputs" name="mailuser" placeholder="Username">
+								<input type="text" class="form-inputs" id="mailuser" name="mailuser" placeholder="Username">
 							</div>
 							<div class="form-group">
 								<input type="password" class="form-inputs" name="password" placeholder="Password">
+								
 							</div>
 							<div class="form-group">
-								<p id="forgotPassword">Forgot Password</p>
+								<p style="display: none" id="forgotPassword">Forgot Password</p>
 							</div>
 
 							<button class="btn btn-dark" type="submit" name="login-submit" id="login-submit-btn">Login</button>
@@ -46,7 +47,7 @@
 						<h1>Welcome</h1>
 						<h2>Please complete to create your account.</h2>
 					</div>
-					<div class="form-wrapper">
+					<div class="form-wrapper" id="signup-form">
 						<form action="#" method="post">
 							<div class="form-row">
 								<div class="form-group col-md-6" id="firstNameInput">
@@ -70,7 +71,7 @@
 							</div>
 							<button class="btn btn-dark" type="submit" name="signup-submit" id="signup-submit-btn">Sign-up</button>
 							<div class="form-group">
-								<p id="signin-redirect-ref">Already have an account? Sign in.</p>
+								<p class="signin-redirect-ref">Already have an account? Sign in.</p>
 							</div>
 						</form>
 					</div>
@@ -82,18 +83,23 @@
 						<h2>Enter your email and we will send a password reset link.</h2>
 					</div>
 					<div class="form-wrapper">
-						<form action="pass-reset.inc.php" method="post">
-							<div class="form-group">
-								<input type="text" class="form-inputs" name="emaii" placeholder="Email">
-							</div>
-							<button class="btn btn-dark" type="submit" name="forgot-password-submit">Send request</button>
-						</form>
+						<span id="info">
+							<form action="#" method="post" id="reset-pass-form">
+								<div class="form-group">
+									<input type="text" class="form-inputs" name="emaii" placeholder="Email">
+								</div>
+								<button class="btn btn-dark" type="submit" name="forgot-password-submit" id="password-reset-submit">Send request</button>
+								<div class="form-group">
+									<p class="signin-redirect-ref">Cancel</p>
+								</div>
+							</form>
+						</span>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-	</body>
+</body>
 
 <script src="main.js"></script>
 </body>
