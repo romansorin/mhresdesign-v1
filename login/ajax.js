b4e21447eba79226c2dd810a962ed6c3fb46d510
Roman Sorin <988244@students.mentorschools.org>
2019-01-17 12:28:09 -0500

    Working ajax/jquery/css styling & logic for login form. need refactor

--- login/ajax.js
+++ login/ajax.js
@@ -1,148 +1,103 @@
-let userEmpty = false;
-let userNull = false;
-let passEmpty = false;
-let passWrong = false;
+const userInputField = $('#mailuser-input-login');
+const passInputField = $('#password-input-login');
+const viewPass = $('#view-pass');
 
-
-const password = $('#password-input-login').val();
-const userInput = $('#mailuser-input-login');
-const passInput = $('#password-input-login');
-const submit = $('#login-btn').val();
+let userNullErrorState = false;
+let invalidCredentialsErrorState = false;
 
 const requiredError = "<span class='input-error inline-error'>This field is required.</span>";
 const minCharsError = "<span class='input-error inline-error'>Password must be a minimum of 5 characters.</span>";
-const userPassMatchError = "<span class='input-error inline-error'>Username and password can not match.</span>";
 const userNullError = "<span class='input-error inline-error'>User does not exist.</span>";
 const invalidCredentialsError = "<span class='input-error inline-error'>Invalid username/password.</span>";
 
-// $('#login-form').submit(function (event) {
-//     event.preventDefault();
-//     $.ajax({
-//         type: "POST",
-//         url: "login.inc.php",
-//         data: {username: username, password: password, submit: submit},
-//         success: function (result) {
-//             // alert(result);
+$('#login-form').submit(function (event) {
+    event.preventDefault();
 
-            /*// username checks
-            usernameChecks();
+    const username = $('#mailuser-input-login').val();
+    const password = $('#password-input-login').val();
+    const submit = $('#login-btn').val();
 
-            if (username === password) {
-                inputErrorClass(passInput, userPassMatchError);
+    $.ajax({
+        type: "POST",
+        url: "login.inc.php",
+        data: {username: username, password: password, submit: submit},
+        success: function (result) {
+            if (result === 'userNullError') {
+                userNullErrorState = true;
+                if (!userInputField.hasClass('input-error')) {
+                    userInputField.addClass('input-error');
+                    userInputField.after(userNullError);
+                } else {
+                    userInputField.next().remove();
+                    userInputField.after(userNullError);
+                }
+            } else if (result === 'invalidCredentialsError') {
+                invalidCredentialsErrorState = true;
+                if (!userInputField.hasClass('input-error')) {
+                    userInputField.addClass('input-error');
+                } else {
+                    userInputField.next().remove();
+                }
+                if (!passInputField.hasClass('input-error')) {
+                    passInputField.addClass('input-error');
+                    viewPass.hide();
+                    passInputField.after(invalidCredentialsError);
+                } else {
+                    userInputField.next().remove();
+                    viewPass.hide();
+                    userInputField.after(invalidCredentialsError);
+                }
+            } else {
+                userNullErrorState = false;
+                invalidCredentialsErrorState = false;
+                if (userInputField.hasClass('input-error')) {
+                    userInputField.next().remove();
+                }
+                if (passInputField.hasClass('input-error')) {
+                    passInputField.next().remove();
+                    viewPass.show();
+                }
             }
-
-            passwordChecks();
-            // password
-
         }
     });
 });
 
-// ajax call to : check if username exists
-// ajax call to : check username and password combination
-/*
-// when user input is clicked off
-userInput.focusout(function () {
-    if (username.length === 0) {
-        userEmpty = true;
-        inputErrorClass(userInput, requiredError);
-    }
-});
-
-// when pass input is clicked off
-passInput.focusout(function () {
-    if (password.length === 0) {
-        inputErrorClass(passInput, requiredError);
-    } else if (password.length < 6) {
-        if (passInput.next().length > 0) {
-            passInput.next().remove();
-        }
-        inputErrorClass(passInput, minCharsError);
-    }
-});
-
-function usernameChecks() {
-    if (username.length === 0 || result === 'userEmpty') {
-        userEmpty = true;
-        inputErrorClass(userInput, requiredError);
-    } else if (result === 'userNull') {
-        userNull = true;
-        inputErrorClass(userInput, userNullError);
-    } else if (userEmpty === false && userNull === false) {
-        inputErrorClass(userInput);
-    }
-}
-
-function inputErrorClass(id, error) {
-    if (id.hasClass('input-error')) {
-        id.removeClass('input-error');
-        id.next().remove();
-    } else {
-        id.addClass('input-error');
-        id.after(error);
-    }
-}
-
-function passwordChecks() {
-    if (password.length === 0 || result === 'passEmpty') {
-        passEmpty = true;
-        inputErrorClass(passInput)
-        $('#password-input-login').addClass('input-error');
-        $('#password-input-login').after(requiredError);
-    } else if (result === 'passWrong') {
-        passWrong = true;
-        $('#password-input-login').addClass('input-error');
-        $('#password-input-login').after(invalidCredentialsError);
-    } else if (passEmpty === false && passWrong === false) {
-        $('#password-input-login').removeClass('input-error');
-        $('#password-input-login').next().remove();
-
-    }
-}
-// On error display, the 'show-hide' button should be hidden and then brought back when there is no error
-/* Client-side validation:
-   1. Check for empty fields [ERROR: This field is required; HIGHLIGHT: Respective fields]
-       a. Username
-       b. Password
-   2. Check that the password is more than 5 characters [ERROR: Password cannot be less than 5 characters; HIGHLIGHT: Password input]
-
-   Server-side validation:
-   1. Check that the username exists [ERROR: User does not exist; HIGHLIGHT: Username input]
-   2. Check that the username and password are correct [ERROR: Invalid username/password combination; HIGHLIGHT: Username and password fields, error message displayed in password]
- */
-
-
-userInput.focusout(function () {
+userInputField.focusout(function () {
     if ($('#mailuser-input-login').val().length === 0) {
-        if (!userInput.hasClass('input-error')) {
-            userInput.addClass('input-error');
-            userInput.after(requiredError);
+        if (!userInputField.hasClass('input-error')) {
+            userInputField.addClass('input-error');
+            userInputField.after(requiredError);
         }
-    } else {
-        userInput.removeClass('input-error');
-        userInput.next().remove();
+    } else if (!userNullErrorState && !invalidCredentialsErrorState) {
+        userInputField.removeClass('input-error');
+        userInputField.next().remove();
     }
 });
 
-passInput.focusout(function() {
-   if ($('#password-input-login').val().length === 0) {
-       if (!passInput.hasClass('input-error')) {
-           passInput.addClass('input-error');
-           passInput.after(requiredError);
-       } else {
-           passInput.next().remove();
-           passInput.after(requiredError);
-       }
-   } else if ($('#password-input-login').val().length < 5) {
-       if (!passInput.hasClass('input-error')) {
-           passInput.addClass('input-error');
-           passInput.after(minCharsError);
-       } else {
-           passInput.next().remove();
-           passInput.after(minCharsError);
-       }
-   } else {
-       passInput.removeClass('input-error');
-       passInput.next().remove();
-   }
+passInputField.focusout(function () {
+    if ($('#password-input-login').val().length === 0) {
+        if (!passInputField.hasClass('input-error')) {
+            passInputField.addClass('input-error');
+            viewPass.hide();
+            passInputField.after(requiredError);
+        } else {
+            passInputField.next().remove();
+            viewPass.hide();
+            passInputField.after(requiredError);
+        }
+    } else if ($('#password-input-login').val().length < 5) {
+        if (!passInputField.hasClass('input-error')) {
+            passInputField.addClass('input-error');
+            viewPass.hide();
+            passInputField.after(minCharsError);
+        } else {
+            passInputField.next().remove();
+            viewPass.hide();
+            passInputField.after(minCharsError);
+        }
+    } else if (!invalidCredentialsErrorState) {
+        passInputField.removeClass('input-error');
+        passInputField.next().remove();
+        viewPass.show();
+    }
 });
--- login/login.inc.php
+++ login/login.inc.php
@@ -3,50 +3,36 @@
 include '../inc/connection/connection.php';
 include '../inc/connection/configs.php';
 
-$conn    = new Connection();
+$conn = new Connection();
 $userPDO = $conn->connectToDb($db_users, $user_users, $pass_users);
-
-$mail_user_error = $pass_error = "";
 
 if (isset($_POST['submit'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-        if (empty($username)) { // this is empty,
-            echo 'userEmpty';
-        } else if (empty($password)) { // this is empty
-            echo 'passEmpty';
+    $query = "SELECT * FROM users WHERE username='$username'";
+    $stmt = $userPDO->query($query);
+    $stmt->execute();
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($row) {
+
+        $password_check = password_verify($password, $row['password']);
+
+        if ($password_check === false) {
+            echo 'invalidCredentialsError';
+        } else if ($password_check === true) {
+            session_start();
+            $_SESSION['userID'] = $row['id'];
+            $_SESSION['username'] = $row['username'];
+
+            header("Location: ../dashboard/index.php?login=success");
+            exit();
         } else {
-            $query = "SELECT * FROM users WHERE username='$username'";
-            $stmt  = $userPDO->query($query);
-            $stmt->execute();
-
-            $row = $stmt->fetch(PDO::FETCH_ASSOC);
-
-            if ($row) {
-
-                $password_check = password_verify($password, $row['password']);
-
-                if ($password_check === false) {
-// this is for if the password is false
-                   // echo "<span class='input-error inline-error'>Invalid email / password combination.</span>";
-                    echo 'passWrong';
-                } else if ($password_check === true) {
-                    session_start();
-                    $_SESSION['userID']   = $row['id'];
-                    $_SESSION['username'] = $row['username'];
-
-                    header("Location: ../dashboard/index.php?login=success");
-                    exit();
-                } else {
-                    header("Location: index.php?login=failure");
-                    exit();
-                }
-            } else {
-// this is if the username does not exist
-               // echo "<span class='input-error inline-error'>Invalid email / password combination.</span>";
-                //echo "<span class='input-error inline-error'>User does not exist.</span>";
-                echo 'userNull';
-            }
+            header("Location: index.php?login=failure");
+            exit();
         }
+    } else {
+        echo 'userNullError';
+    }
 }
