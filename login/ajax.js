e695688b938170148f1bca6b73e4eba05045c71a
Roman Sorin <988244@students.mentorschools.org>
2019-01-17 22:56:08 -0500

    cleaned up ajax call nicely through refactoring into functions

--- login/ajax.js
+++ login/ajax.js
@@ -1,6 +1,6 @@
-const userInputField = document.getElementById('mailuser-input-login');
-const passInputField = document.getElementById('password-input-login');
-const viewPass = document.getElementById('view-pass');
+const userInputField = $("#mailuser-input-login");
+const passInputField = $("#password-input-login");
+let viewPass = $("#view-pass");
 
 let username;
 let password;
@@ -12,6 +12,9 @@ let errorStates = {
 };
 
 // @TODO: make placeholders red on error
+// @TODO: replace if-else's with switch where reasonable
+// @TODO: handle case of clicking submit button without any input?
+// @TODO: make username and password field input checks (for null, minchars, etc) into independent functions so they can possibly be used in the submit event
 
 const errors = {
     required: "<span class='input-error inline-error'>This field is required.</span>",
@@ -28,51 +31,72 @@ $("#login-form").submit(function (event) {
     password = $("#password-input-login").val();
     submit = $("#login-btn").val();
 
-
     $.ajax({
         type: "POST",
         url: "login.inc.php",
         data: {username: username, password: password, submit: submit},
         success: function (result) {
-            if (result === errors.userNullReturned) {
-                errorStates.userNull = true;
-                if (!userInputField.hasClass("input-error")) {
-                    userInputField.addClass("input-error");
-                    userInputField.after(errors.userNull);
-                } else {
-                    userInputField.next().remove();
-                    userInputField.after(errors.userNull);
-                }
-            } else if (result === errors.invalidCredentialsReturned) {
-                errorStates.invalidCredentials = true;
-                if (!userInputField.hasClass("input-error")) {
-                    userInputField.addClass("input-error");
-                } else {
-                    userInputField.next().remove();
-                }
-                if (!passInputField.hasClass("input-error")) {
-                    passInputField.addClass("input-error");
-                    viewPass.hide();
-                    passInputField.after(errors.invalidCredentials);
-                } else {
-                    userInputField.next().remove();
-                    viewPass.hide();
-                    userInputField.after(errors.invalidCredentials);
-                }
-            } else {
-                errorStates.userNull = false;
-                errorStates.invalidCredentials = false;
-                if (userInputField.hasClass("input-error")) {
-                    userInputField.next().remove();
-                }
-                if (passInputField.hasClass("input-error")) {
-                    passInputField.next().remove();
-                    viewPass.show();
-                }
+            switch (result) {
+                case errors.userNullReturned:
+                    userNullCheck();
+                    break;
+                case errors.invalidCredentialsReturned:
+                    invalidCredentialsCheck();
+                    break;
+                default:
+                    validChecks();
+                    break;
             }
         }
     });
-});
+})
+;
+
+function userNullCheck() {
+    errorStates.userNull = true;
+    if (!userInputField.hasClass("input-error")) {
+        userInputField.addClass("input-error");
+        userInputField.after(errors.userNull);
+    } else {
+        userInputField.next().remove();
+        userInputField.after(errors.userNull);
+    }
+}
+
+function invalidCredentialsCheck() {
+    errorStates.invalidCredentials = true;
+    if (!userInputField.hasClass("input-error")) {
+
+        userInputField.addClass("input-error");
+    } else {
+
+        userInputField.next().remove();
+    }
+    if (!passInputField.hasClass("input-error")) {
+
+        passInputField.addClass("input-error");
+        viewPass.hide();
+        passInputField.after(errors.invalidCredentials);
+
+    } else {
+
+        userInputField.next().remove();
+        viewPass.hide();
+        userInputField.after(errors.invalidCredentials);
+    }
+}
+
+function validChecks() {
+    errorStates.userNull = false;
+    errorStates.invalidCredentials = false;
+    if (userInputField.hasClass("input-error")) {
+        userInputField.next().remove();
+    }
+    if (passInputField.hasClass("input-error")) {
+        passInputField.next().remove();
+        viewPass.show();
+    }
+}
 
 userInputField.focusout(function () {
     username = $("#mailuser-input-login").val();
@@ -117,3 +141,15 @@ passInputField.focusout(function () {
         viewPass.show();
     }
 });
+
+viewPass.click(function (event) {
+    event.preventDefault();
+    if (passInputField.attr('type') == 'password') {
+        passInputField.attr('type', 'text');
+        $(this).find($(".far")).removeClass('fa-eye-slash').addClass('fa-eye');
+    } else {
+        passInputField.attr('type', 'password');
+        $(this).find($(".far")).removeClass('fa-eye').addClass('fa-eye-slash');
+    }
+});
+
--- login/main.js
+++ login/main.js
@@ -10,15 +10,3 @@ $('a[href="#forgot-password"]').click(function(e) {
     $('.content').hide();
     $('#forgot-password').show();
 });
-/* soon to be implemented */
-$('#view-pass').click(function(e) {
-    e.preventDefault();
-    let $pass = $('#password-input-login');
-    if ($pass.attr('type') == 'password') {
-        $pass.attr('type', 'text');
-        $(this).find($(".far")).removeClass('fa-eye-slash').addClass('fa-eye');
-    } else {
-        $pass.attr('type', 'password');
-        $(this).find($(".far")).removeClass('fa-eye').addClass('fa-eye-slash');
-    }
-});
