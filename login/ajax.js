31b0250e106d2ecbe1b1dfd12b5f93c9d1a35bf7
Roman Sorin <988244@students.mentorschools.org>
2019-01-18 09:24:24 -0500

    Added in comments and more switch statements

--- login/ajax.js
+++ login/ajax.js
@@ -1,3 +1,4 @@
+// Define field variables for later use in functions
 const userInputField = $("#mailuser-input-login");
 const passInputField = $("#password-input-login");
 const login = $("#login-btn");
@@ -7,6 +8,7 @@ let username;
 let password;
 let submit;
 
+// ErrorStates array holds the current error shown later, used for checking validity and status
 let errorStates = {
     userInput: false,
     userNull: false,
@@ -14,12 +16,10 @@ let errorStates = {
 };
 
 // @TODO: make placeholders red on error
-// @TODO: replace if-else's with switch where reasonable
-// @TODO: handle case of clicking submit button without any input?
-// @TODO: make username and password field input checks (for null, minchars, etc) into independent functions so they can possibly be used in the submit event
 // @TODO: commenting
 // @TODO: figure out why invalid username / password is so slow responding..
 
+// Errors array to easily access the provided errors and the error codes which are returned from login.inc.php
 const errors = {
     required: "<span class='input-error inline-error'>This field is required.</span>",
     minChars: "<span class='input-error inline-error'>Password must be a minimum of 5 characters.</span>",
@@ -31,6 +31,7 @@ const errors = {
 
 $("#login-form").submit(function (event) {
     event.preventDefault();
+    // Set the value here so the values are updated
     username = userInputField.val();
     password = passInputField.val();
     submit = login.val();
@@ -40,8 +41,10 @@ $("#login-form").submit(function (event) {
         url: "login.inc.php",
         data: {username: username, password: password, submit: submit},
         success: function (result) {
+            /* In the case that a user submits the login form without entering any fields, check if these fields are empty. If they are, respond with a "required" error */
             userFieldChecks();
             passFieldChecks();
+            /* This switch checks the result which is returned from login.inc.php in the form of a string */
             switch (result) {
                 case errors.userNullReturned:
                     userNullCheck();
@@ -58,68 +61,79 @@ $("#login-form").submit(function (event) {
 })
 ;
 
+/** [userNullCheck: Checks if the username does not exist in the database] */
 function userNullCheck() {
     errorStates.userNull = true;
-    if (!userInputField.hasClass("input-error")) {
-        userInputField.addClass("input-error");
-        if (!errorStates.userInput) {
-            userInputField.after(errors.userNull);
-        }
-    } else {
-        if (!errorStates.userInput) {
-            userInputField.next().remove();
-            userInputField.after(errors.userNull);
-        }
+    switch (userInputField.hasClass("input-error")) { // Check if error class is already applied
+        case false: // If it isn't, add the class and check if the "userInput" error is already defined
+            userInputField.addClass("input-error");
+            if (errorStates.userInput === false)
+                userInputField.after(errors.userNull);
+            break;
+        case true: // If the class exist, remove the error message and add the userNull error message instead
+            if (errorStates.userInput === false) {
+                userInputField.next().remove();
+                userInputField.after(errors.userNull);
+            }
+            break;
     }
 }
-
+/** [invalidCredentialsCheck: Check if username/password are valid and matching] */
 function invalidCredentialsCheck() {
     errorStates.invalidCredentials = true;
-    if (!userInputField.hasClass("input-error")) {
-        userInputField.addClass("input-error");
-    } else {
-        if (!errorStates.userInput) {
-            userInputField.next().remove();
-        }
+    switch (userInputField.hasClass("input-error")) {
+        case false:
+            if (!userInputField.hasClass("input-error"))
+                userInputField.addClass("input-error");
+            break;
+        case true:
+            if (!errorStates.userInput)
+                userInputField.next().remove();
+            break;
     }
-    if (!passInputField.hasClass("input-error")) {
-        passInputField.addClass("input-error");
-        viewPass.hide();
-        passInputField.after(errors.invalidCredentials);
-    } else {
-        userInputField.next().remove();
-        viewPass.hide();
-        userInputField.after(errors.invalidCredentials);
+    switch (passInputField.hasClass("input-error")) {
+        case false:
+            passInputField.addClass("input-error");
+            viewPass.hide();
+            passInputField.after(errors.invalidCredentials);
+            break;
+        case true:
+            userInputField.next().remove();
+            viewPass.hide();
+            userInputField.after(errors.invalidCredentials);
     }
 }
-
+/** [validChecks: Executed if all other checks are false, resets the error fields] */
 function validChecks() {
     // @TODO: utilize a for-each loop to assign false?
     errorStates.userInput = false;
     errorStates.userNull = false;
     errorStates.invalidCredentials = false;
-    if (userInputField.hasClass("input-error")) {
+    if (userInputField.hasClass("input-error"))
         userInputField.next().remove();
-    }
     if (passInputField.hasClass("input-error")) {
         passInputField.next().remove();
         viewPass.show();
     }
 }
 
+/** [userFieldChecks: Check for empty username fields and error states] */
 function userFieldChecks() {
     username = userInputField.val();
     if (username.length === 0) {
         errorStates.userInput = true;
-        if (!userInputField.hasClass("input-error")) {
-            userInputField.addClass("input-error");
-            userInputField.after(errors.required);
-        } else {
-            userInputField.next().remove();
-            userInputField.after(errors.required);
+        switch (userInputField.hasClass("input-error")) {
+            case false:
+                userInputField.addClass("input-error");
+                userInputField.after(errors.required);
+                break;
+            case true:
+                userInputField.next().remove();
+                userInputField.after(errors.required);
+                break;
         }
-        // @TODO: maybe something to test the collective state of all elements in the array?
-    } else if (!errorStates.userNull && !errorStates.invalidCredentials && !errorStates.userInput) {
+// @TODO: maybe something to test the collective state of all elements in the array?
+    } else if (errorStates.userNull === false && errorStates.invalidCredentials === false && errorStates.userInput === false) {
         userInputField.removeClass("input-error");
         userInputField.next().remove();
     } else {
@@ -128,30 +142,36 @@ function userFieldChecks() {
         userInputField.next().remove();
     }
 }
-
+/** [passFieldChecks: Check for empty fields & character length] */
 function passFieldChecks() {
     password = passInputField.val();
     if (password.length === 0) {
-        if (!passInputField.hasClass("input-error")) {
-            passInputField.addClass("input-error");
-            viewPass.hide();
-            passInputField.after(errors.required);
-        } else {
-            passInputField.next().remove();
-            viewPass.hide();
-            passInputField.after(errors.required);
+        switch (passInputField.hasClass("input-error")) {
+            case false:
+                passInputField.addClass("input-error");
+                viewPass.hide();
+                passInputField.after(errors.required);
+                break;
+            case true:
+                passInputField.next().remove();
+                viewPass.hide();
+                passInputField.after(errors.required);
+                break;
         }
     } else if (password.length < 5) {
-        if (!passInputField.hasClass("input-error")) {
-            passInputField.addClass("input-error");
-            viewPass.hide();
-            passInputField.after(errors.minChars);
-        } else {
-            passInputField.next().remove();
-            viewPass.hide();
-            passInputField.after(errors.minChars);
+        switch (passInputField.hasClass("input-error")) {
+            case false:
+                passInputField.addClass("input-error");
+                viewPass.hide();
+                passInputField.after(errors.minChars);
+                break;
+            case true:
+                passInputField.next().remove();
+                viewPass.hide();
+                passInputField.after(errors.minChars);
+                break;
         }
-    } else if (!errorStates.invalidCredentials) {
+    } else if (errorStates.invalidCredentials === false) {
         if (passInputField.hasClass("input-error")) {
             passInputField.removeClass("input-error");
             passInputField.next().remove();
@@ -159,7 +179,7 @@ function passFieldChecks() {
         viewPass.show();
     }
 }
-
+/** [Run these when input fields lose focus to validate the input] */
 userInputField.focusout(function () {
     userFieldChecks();
 });
